48707608fb5174f1e9c983851953c3112e621084
hitaboy <hitaboy@gmail.com>
2014-02-27 12:10:36 +0100

    Clean up

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 					<!-- logo -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logoHG.png" alt="Logo" class="logo-img">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
 						</a>
 					</div>
 					<!-- /logo -->
